6cad40929966bd5152e297307515e8ede578abb2
Andrdav <andreigherghe19@yahoo.com>
2022-03-16 17:24:12 +0100

    Register/Login

--- app/src/main/java/co/tournam/schedule/MainActivity.java
+++ app/src/main/java/co/tournam/schedule/MainActivity.java
@@ -1,25 +1,37 @@
 package co.tournam.schedule;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 import co.tournam.models.FakeData;
 import co.tournam.ui.matchlist.SummaryMatchList;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        FakeData data = new FakeData();
-        addContentView(
-                new SummaryMatchList(
-                        this.getApplicationContext(),
-                        data.tournament.getStages().get(0).getRounds().get(0).getMatches()),
-                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        setContentView(R.layout.activity_main);
+           FakeData data = new FakeData();
+           addContentView(
+                  new SummaryMatchList(
+                           this.getApplicationContext(),
+                           data.tournament.getStages().get(0).getRounds().get(0).getMatches()),
+                   new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
     }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+
 }
---
+++ app/src/main/java/co/tournam/schedule/Register.java
@@ -0,0 +1,111 @@
+package co.tournam.schedule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity {
+
+    EditText userName, email, password, gamerTag, repeatPassword;
+    Button registerButton, loginButton;
+    private FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userName = findViewById(R.id.editTextTextPersonName);
+        email = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editPassword);
+        repeatPassword = findViewById(R.id.editRepeatPassword);
+        gamerTag = findViewById(R.id.editTag);
+        registerButton = findViewById(R.id.SignUp);
+        loginButton = findViewById(R.id.LogIn);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String sEmail = email.getText().toString().trim();
+                String sPassword = password.getText().toString().trim();
+                String sRepeatPassword = repeatPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(sEmail)) {
+                    email.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(sPassword)) {
+                    password.setError("Password is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(sRepeatPassword)) {
+                    repeatPassword.setError("Password must be verified.");
+                }
+
+                if (sPassword.length() < 5) { //settable password length
+                    password.setError("Password must have appropriate number of characters.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //register user
+                mAuth.createUserWithEmailAndPassword(sEmail, sPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Not able to Create an User: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+}
