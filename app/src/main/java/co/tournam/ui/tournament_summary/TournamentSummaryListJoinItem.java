895cad978417487b85366878c12801f8bc4bec4f
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2022-04-14 00:59:42 +0200

    Small fix for displaying

--- app/src/main/java/co/tournam/ui/tournament_summary/TournamentSummaryListJoinItem.java
+++ app/src/main/java/co/tournam/ui/tournament_summary/TournamentSummaryListJoinItem.java
@@ -123,6 +123,7 @@ public class TournamentSummaryListJoinItem extends LinearLayout {
     }
 
     public double distance(double lat1, double lat2, double lon1, double lon2, double el1, double el2) {
+        final int R = 6371;
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
